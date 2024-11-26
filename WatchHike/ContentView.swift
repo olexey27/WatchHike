cdc633205ca4cc0089374ace69d9b15aec1bcdf2
olexey27 <allxyog@gmail.com>
2024-11-26 14:56:07 +0100

    Initial Commit

---
+++ WatchHike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchHike
+//
+//  Created by Alexej K on 11/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchHike/WatchHikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchHikeApp.swift
+//  WatchHike
+//
+//  Created by Alexej K on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchHikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
