b54ea6e6531bf6687ab774dc842775a732769827
olexey27 <allxyog@gmail.com>
2025-04-21 21:32:12 +0100

    motion animation

---
+++ WatchHike/Component/CustomCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCircleView.swift
+//  WatchHike
+//
+//  Created by Alexej K on 21/04/2025.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
---
+++ WatchHike/Component/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  WatchHike
+//
+//  Created by Alexej K on 21/04/2025.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
