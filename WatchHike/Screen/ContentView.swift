ef81164a42eff9931809cedaffb8b0406b4aaeac
olexey27 <allxyog@gmail.com>
2025-04-23 23:26:31 +0100

    sheet

---
+++ WatchHike/Screen/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WatchHike
+//
+//  Created by Alexej K on 11/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchHike/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  WatchHike
+//
+//  Created by Alexej K on 23/04/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
