9f4d7023dc86b6cb834a3402bd5c6616b906973e
olexey27 <allxyog@gmail.com>
2025-04-26 22:52:20 +0100

    SwiftUI List and Labeled Content

---
+++ WatchHike/Component/CustomListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListRowView.swift
+//  WatchHike
+//
+//  Created by Alexej K on 26/04/2025.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListRowView()
+}
